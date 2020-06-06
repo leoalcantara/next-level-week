d48c3b105f80ed87215b5922ea6ae8498a1dfec2
leoalcantara <leonidio.ti@gmail.com>
2020-06-05 21:45:08 -0300

    created database.ts file

---
+++ server/src/database/connection.ts
@@ -0,0 +1,9 @@
+import knex from 'knex';
+import path from 'path';
+
+const connection = knex({
+    client: 'sqlite',
+    connection: {
+        filename: path.resolve(__dirname, 'database.sqlite'),
+    }
+});
