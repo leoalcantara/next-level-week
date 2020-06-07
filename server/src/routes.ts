8dd7614f44a41617d83e61430f29482776159f07
leoalcantara <leonidio.ti@gmail.com>
2020-06-07 20:36:50 -0300

    Created routes for items

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-    return response.json({message: 'Hello Word'});
+routes.get('/items', async (request, response) => {
+    const items = await knex('items').select('*');
+
+    const serializeditems = items.map(item =>{
+      return{
+        title: item.name,
+        image_url: `http://localhost:3333/uploads/${item.image}`,
+      }
+    })
+
+    return response.json(serializeditems);
   });
 
   export default routes;
