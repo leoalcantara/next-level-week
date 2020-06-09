e62a350d9944058dcdd8d12e0adae96217f5835d
leoalcantara <leonidio.ti@gmail.com>
2020-06-09 16:35:11 -0300

    Integrating the map with the form

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link }  from 'react-router-dom';
-import { FiArrowLeft} from 'react-icons/fi'
+import { FiArrowLeft} from 'react-icons/fi';
+import { Map, TileLayer, Marker} from 'react-leaflet';
 
 import './style.css';
 import logo from '../../assets/logo.svg'
@@ -59,22 +60,31 @@ const CreatePoint = () => {
                         <h2>Endereço</h2>
                         <span>Selecione um endereço no mapa</span>
                     </legend>
+
+                    <Map center={[-27.20922052, -49.6401092]} zoom={15}>
+                        <TileLayer
+                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Marker position={[-27.20922052, -49.6401092]}/>
+
+                    </Map>
+
                     <div className="field-group">
                         <div className="field">
-                            <label htmlFor="uf">Estado (UF)"</label>
+                            <label htmlFor="uf">Estado (UF)</label>
                             <select name="uf" id="uf">
                                 <option value="0">Selecione uma UF</option>
                             </select>
-                        </div>
-                    </div>
-                    <div className="field-group">
+                        </div>                  
+                        
                         <div className="field">
                             <label htmlFor="city">Cidade</label>
                             <select name="city" id="city">
                                 <option value="0">Selecione uma Cidade</option>
                             </select>
                         </div>
-                    </div>
+                        </div>
                 </fieldset>
 
                 <fieldset>
