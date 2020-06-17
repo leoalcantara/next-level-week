52efe6b27361a3612b35305d8faf9615e909f12b
leoalcantara <leonidio.ti@gmail.com>
2020-06-17 17:05:53 -0300

    Homepage Mobile

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { Feather as Icon } from '@expo/vector-icons'
 import { View, ImageBackground, Image, StyleSheet, Text} from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+
 
 const Home = ( ) =>{
     return (
@@ -13,7 +16,19 @@ const Home = ( ) =>{
             <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
             <Text style={styles.description}>Ajudamos as pessoas a encontrarem pontos de coleta de forma eficiênte</Text>
           </View>  
-        
+
+          <View style={styles.footer}>
+            <RectButton style={styles.button} onPress={()=>{}}>
+              <View style={styles.buttonIcon}>
+                <Text>
+                  <Icon name= 'arrow-right' color='#FFF' size={24} ></Icon>
+                </Text>
+              </View>              
+              <Text style={styles.buttonText}>
+                Entrar
+              </Text>    
+            </RectButton>
+          </View>         
         </ImageBackground>
     );
 };
