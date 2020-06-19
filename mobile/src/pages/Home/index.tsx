9437d0939fd0eb3e3e044e7452155a23cd267370
leoalcantara <leonidio.ti@gmail.com>
2020-06-19 20:06:59 -0300

    creating navigation routes

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -2,9 +2,17 @@ import React from 'react';
 import { Feather as Icon } from '@expo/vector-icons'
 import { View, ImageBackground, Image, StyleSheet, Text} from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation} from '@react-navigation/native';
+
 
 
 const Home = ( ) =>{
+  const navigation = useNavigation();
+
+  function handleNavigateToPoints(){
+    navigation.navigate('Points')
+  }
+
     return (
         <ImageBackground 
             source={require('../../assets/home-background.png')}
@@ -18,14 +26,14 @@ const Home = ( ) =>{
           </View>  
 
           <View style={styles.footer}>
-            <RectButton style={styles.button} onPress={()=>{}}>
+            <RectButton style={styles.button} onPress={handleNavigateToPoints}>
               <View style={styles.buttonIcon}>
                 <Text>
-                  <Icon name= 'arrow-right' color='#FFF' size={24} ></Icon>
+                  <Icon name= 'arrow-right' color='#FFF' size={ 24 } ></Icon>
                 </Text>
               </View>              
               <Text style={styles.buttonText}>
-                Entrar
+                  Entrar
               </Text>    
             </RectButton>
           </View>         
@@ -36,8 +44,7 @@ const Home = ( ) =>{
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      padding: 32,
-      backgroundColor: '#F0F0F5'
+      padding: 32,      
     },
   
     main: {
