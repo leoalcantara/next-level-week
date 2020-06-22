c4a999decdf0ce2632614267e27aca878b17b4cc
leoalcantara <leonidio.ti@gmail.com>
2020-06-21 21:22:35 -0300

    transferring the data to the collection points page

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -1,15 +1,51 @@
-import React from 'react';
-import { useNavigation } from '@react-navigation/native';
+import React, { useEffect, useState } from 'react';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import { Feather as Icon , FontAwesome } from '@expo/vector-icons';
 import { View, StyleSheet, TouchableOpacity, Image , Text, SafeAreaView } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import api from '../../services/api';
+
+interface Params{
+  point_id: number;
+}
+
+interface Data {
+  point:{
+    image: string;
+    name: string;
+    email: string;
+    whatsapp: string;
+    city: string;
+    uf: string;
+  }
+  items: {
+    title: string;
+  }[];
+
+}
 
 const Detail = () => {
+    const [data, setData]=useState<Data>({} as Data);
+
     const navigation = useNavigation();
+    const route = useRoute();
+
+    const routeParams = route.params as Params;
+
+
+    useEffect(() =>{
+      api.get(`points/${routeParams.point_id}`).then(response =>{
+        setData(response.data);
+      });
+    },[]);
 
     function handleNavigateBack(){
         navigation.goBack();
-    };
+    }
+
+    if (!data.point){
+      return null;
+    }
 
 return (
     <SafeAreaView style={{flex: 1}}>
@@ -18,14 +54,14 @@ return (
                <Icon name="arrow-left" size={20} color="#34cb79"/>
            </TouchableOpacity>
 
-           <Image style={styles.pointImage} source={{ uri: 'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60' }}/>
+           <Image style={styles.pointImage} source={{ uri: data.point.image }}/>
            
-           <Text style={styles.pointName}>Mercado do João</Text>
-           <Text style={styles.pointItems}>Lâmpadas e óleo de cozinha</Text>
+           <Text style={styles.pointName}>{data.point.name}</Text>
+           <Text style={styles.pointItems}>{data.items.map(item =>item.title).join(', ')}</Text>
 
            <View style={styles.address}>
                <Text style={styles.addressTitle}>Endereço</Text>
-               <Text style={styles.addressContent}>Itaperuna - Rj</Text>
+               <Text style={styles.addressContent}>{data.point.city}, {data.point.uf}</Text>
            </View>
         </View>
         <View style={styles.footer}>
--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -15,8 +15,18 @@ interface Item{
   image_url: string;
 }
 
+interface Point{
+  id: number;
+  name:string,
+  image: string,
+  latitude: number,
+  longitude: number,
+  
+}
+
 const Points = () => {
   const [items, setItems] = useState<Item[]>([]);
+  const [points, setPoints] = useState<Point[]>([]);
   const [selectedItems, setSelectedItems] = useState<number[]>([]);
   
   const [initialPosition, setInitialPosition] = useState<[number, number]>([0,0]);
@@ -52,13 +62,25 @@ const Points = () => {
     })
 
   }, []);
+
+  useEffect(()=>{
+    api.get('points',{
+      params: {
+        city: 'Itaperuna',
+        uf: 'RJ',
+        items: [1]
+      }
+    }).then(response =>{
+      setPoints(response.data);
+    })
+  },[]);
     
    function handleNavigateBack(){
         navigation.goBack();
     };
 
-    function handleNavigateToDetail(){
-      navigation.navigate('Detail');
+    function handleNavigateToDetail(id: number){
+      navigation.navigate('Detail', { point_id: id});
     }
 
     function handleSelectItem(id: number){
@@ -97,19 +119,24 @@ return (
                 longitudeDelta:0.014,
               }}
             >
-              <Marker 
+              {points.map(point =>(
+                <Marker 
+                key={String(point.id)}
                 style={styles.mapMarker}
-                onPress={handleNavigateToDetail}
+                onPress={() => handleNavigateToDetail(point.id)}
                 coordinate={{
-                  latitude: -21.2106271,
-                  longitude:-41.8898371,
+                  latitude: point.latitude,
+                  longitude: point.longitude,
                   }}
               > 
                 <View style={styles.mapMarkerContainer}>
-                  <Image style={styles.mapMarkerImage} source={{ uri: 'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60' }}/>
-                  <Text style={styles.mapMarkerTitle}> Mercado</Text>  
+                  <Image style={styles.mapMarkerImage} source={{ uri: point.image }}/>
+                  <Text style={styles.mapMarkerTitle}> {point.name}</Text>  
                 </View>    
               </Marker>
+
+              ))}
+              
             </ViewMap>
           ) }
         </View>
