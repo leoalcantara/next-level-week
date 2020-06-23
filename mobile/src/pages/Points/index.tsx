29298cae575fd3ac99fad08c0837253767f3baf4
leoalcantara <leonidio.ti@gmail.com>
2020-06-22 22:17:19 -0300

    uf search and city

--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Constants  from 'expo-constants';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import { Feather as Icon } from '@expo/vector-icons';
 import { View, ScrollView, Text, StyleSheet, TouchableOpacity, Image, SafeAreaView, Alert } from 'react-native';
 import ViewMap, {Marker} from 'react-native-maps';
@@ -17,21 +17,29 @@ interface Item{
 
 interface Point{
   id: number;
-  name:string,
-  image: string,
-  latitude: number,
-  longitude: number,
+  name:string;
+  image: string;
+  latitude: number;
+  longitude: number;
   
+}
+
+interface Params {
+  uf: string;
+  city: string;
 }
 
 const Points = () => {
   const [items, setItems] = useState<Item[]>([]);
   const [points, setPoints] = useState<Point[]>([]);
   const [selectedItems, setSelectedItems] = useState<number[]>([]);
-  
+
   const [initialPosition, setInitialPosition] = useState<[number, number]>([0,0]);
 
   const navigation = useNavigation(); 
+  const route = useRoute();
+
+  const routeParams = route.params as Params;
 
   useEffect(()=>{
     async function loadPosition(){
@@ -66,14 +74,15 @@ const Points = () => {
   useEffect(()=>{
     api.get('points',{
       params: {
-        city: 'Itaperuna',
-        uf: 'RJ',
-        items: [1]
+        city: routeParams.city,
+        uf: routeParams.uf,
+        items: selectedItems,
       }
+
     }).then(response =>{
       setPoints(response.data);
     })
-  },[]);
+  },[selectedItems]);
     
    function handleNavigateBack(){
         navigation.goBack();
