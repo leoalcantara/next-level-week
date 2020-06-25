62034f9c2115aa4e4331f50ecf9ea68031bc02eb
leoalcantara <leonidio.ti@gmail.com>
2020-06-25 15:27:11 -0300

    uploading the image

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {  Route, BrowserRouter, RouteProps} from 'react-router-dom';
+import {  Route, BrowserRouter} from 'react-router-dom';
 
 import Home from './pages/Home';
 import CreatePoint from './pages/CreatePoint';
